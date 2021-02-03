08a5d67edfb89920c1822f01683f00ddef8e6968
Francois Polo <andasan@gmail.com>
2021-02-03 12:22:03 -0800

    testing absolute url

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production'
+const PORT = process.env.PORT
 
-export const server = dev ? 'http://localhost:3000' : ''
+export const server = dev ? 'http://localhost:3000' : `http://localhost:${PORT}`
